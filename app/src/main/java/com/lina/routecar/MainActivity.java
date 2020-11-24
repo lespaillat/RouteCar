b52af7504b32da6d64dbfb8a54814fb879f186bd
lespaillat <lespaillatj01@educantabria.es>
2020-11-24 10:56:37 +0100

    He sumado un action bar, he agregado navegación entre el main y la seleccion de login o registro

--- app/src/main/java/com/lina/routecar/MainActivity.java
+++ app/src/main/java/com/lina/routecar/MainActivity.java
@@ -2,13 +2,41 @@ package com.lina.routecar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mButtonIAmClient;
+    Button mButtonIAmDriver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonIAmClient = findViewById(R.id.btnIAmClient);
+        mButtonIAmDriver = findViewById(R.id.btnIAmDriver);
+
+        mButtonIAmClient.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                goToSelectAuth();
+            }
+        });
+
+        mButtonIAmDriver.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                goToSelectAuth();
+            }
+        });
+    }
+
+    private void goToSelectAuth() {
+        Intent intent = new Intent(MainActivity.this,SelectOptionAuthActivity2.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/lina/routecar/SelectOptionAuthActivity2.java
+++ app/src/main/java/com/lina/routecar/SelectOptionAuthActivity2.java
@@ -1,14 +1,23 @@
 package com.lina.routecar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SelectOptionAuthActivity2 extends AppCompatActivity {
 
+    Toolbar mToolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth2);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Selecciona Opción");
+        //para ayadir el boton de ir hacia atras
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
